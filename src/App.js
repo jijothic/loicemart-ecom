4b3500bbb3ff67202bd2afa79d3d53134bd542be
jijothic <george.jijothic@gmail.com`>
2020-08-22 18:36:18 +0300

    Add app entry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+
         <p>
           Something  <code>Great</code> is coming 😋.
         </p>
-        
+
       </header>
     </div>
   );
